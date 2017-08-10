d9ff87bf94c0a69044a278eddb75f26a383374ee
piaoliu <441594700@qq.com>
2017-08-10 15:56:41 +0800

    stack and queue by Rust

---
+++ Queue/Rust/queue/src/main.rs
@@ -0,0 +1,91 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
+#[derive(Debug, Clone)]
+struct Node<T> {
+    next: Option<Rc<RefCell<Node<T>>>>,
+    element: T,
+}
+
+#[derive(Debug)]
+struct Queue<T> {
+    head: Option<Rc<RefCell<Node<T>>>>,
+    tail: Option<Rc<RefCell<Node<T>>>>,
+    len: u32,
+}
+
+impl<T> Node<T> {
+    fn new_with_data(data: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Node { next: None, element: data }))
+    }
+}
+
+impl<T> Queue<T> {
+    fn new() -> Queue<T> {
+        Queue {
+            head: None,
+            tail: None,
+            len: 0,
+        }
+    }
+
+    fn push(&mut self, data: T) {
+        let node = Node::new_with_data(data);
+        match self.tail.take() {
+            None => {
+                self.tail = Some(node.clone());
+                self.head = Some(node);
+            }
+            Some(old_tail) => {
+                old_tail.borrow_mut().next = Some(node.clone());
+                self.tail = Some(node);
+            }
+        }
+        self.len += 1;
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        if self.len == 0 {
+            return None;
+        }
+        self.head.take().map(|old_head| {
+            match old_head.borrow_mut().next.take() {
+                Some(new_head) => {
+                    self.head = Some(new_head);
+                    self.len -= 1;
+                }
+                None => {
+                    self.tail.take();
+                    self.len -= 1;
+                }
+            }
+            Rc::try_unwrap(old_head).ok().unwrap().into_inner().element
+        })
+    }
+}
+
+fn main() {
+    #[derive(PartialEq,Eq,Debug)]
+    struct TestStruct {
+        a: i32,
+    }
+
+    let a = TestStruct{ a: 5 };
+    let b = TestStruct{ a: 9 };
+
+    let mut s = Queue::<&TestStruct>::new();
+    assert_eq!(s.pop(), None);
+
+    s.push(&a);
+    s.push(&b);
+    println!("{:?} {}", s, s.len);
+
+    assert_eq!(s.pop(), Some(&a));
+    println!("{:?} {}", s, s.len);
+
+    assert_eq!(s.pop(), Some(&b));
+    println!("{:?} {}", s, s.len);
+
+    assert_eq!(s.pop(), None);
+    println!("{:?} {}", s, s.len);
+}
