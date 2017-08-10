34f51e602f12321fef673e2b54726964a169930f
piaoliu <441594700@qq.com>
2017-08-10 20:19:09 +0800

    double linked list

---
+++ LinearList/Rust/doublelinkedlist/src/main.rs
@@ -0,0 +1,155 @@
+use std::cell::{Ref, RefMut, RefCell};
+use std::rc::Rc;
+
+struct Node<T> {
+    elem: T,
+    next: Option<Rc<RefCell<Node<T>>>>,
+    prev: Option<Rc<RefCell<Node<T>>>>,
+}
+
+struct List<T> {
+    head: Option<Rc<RefCell<Node<T>>>>,
+    tail: Option<Rc<RefCell<Node<T>>>>,
+    length: u32,
+}
+
+struct IntoIter<T>(List<T>);
+
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Node {
+            elem: elem,
+            next: None,
+            prev: None,
+        }))
+    }
+}
+
+impl<T> List<T> {
+    fn new() -> Self {
+        List { head: None, tail: None, length: 0 }
+    }
+
+    fn push_front(&mut self, elem: T) {
+        let new_head = Node::new(elem);
+        match self.head.take() {
+            Some(old_head) => {
+                old_head.borrow_mut().prev = Some(new_head.clone());
+                new_head.borrow_mut().next = Some(old_head);
+                self.head = Some(new_head);
+            }
+            None => {
+                self.tail = Some(new_head.clone());
+                self.head = Some(new_head);
+            }
+        }
+        self.length += 1;
+    }
+
+    fn push_back(&mut self, elem: T) {
+        let new_tail = Node::new(elem);
+        match self.tail.take() {
+            Some(old_tail) => {
+                old_tail.borrow_mut().next = Some(new_tail.clone());
+                new_tail.borrow_mut().prev = Some(old_tail);
+                self.tail = Some(new_tail);
+            }
+            None => {
+                self.head = Some(new_tail.clone());
+                self.tail = Some(new_tail);
+            }
+        }
+        self.length += 1;
+    }
+
+    fn pop_front(&mut self) -> Option<T> {
+        if self.length == 0 {
+            return None
+        }
+        self.head.take().map(|old_head| {
+            match old_head.borrow_mut().next.take() {
+                Some(new_head) => {
+                    new_head.borrow_mut().prev.take();
+                    self.head = Some(new_head);
+                }
+                None => {
+                    self.tail.take();
+                }
+            }
+            self.length -= 1;
+            Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
+        })
+    }
+
+    fn pop_back(&mut self) -> Option<T> {
+        if self.length == 0 {
+            return None
+        }
+        self.tail.take().map(|old_tail| {
+            match old_tail.borrow_mut().prev.take() {
+                Some(new_tail) => {
+                    new_tail.borrow_mut().next.take();
+                    self.tail = Some(new_tail);
+                }
+                None => {
+                    self.head.take();
+                }
+            }
+            self.length -= 1;
+            Rc::try_unwrap(old_tail).ok().unwrap().into_inner().elem
+        })
+    }
+
+    fn peek_front(&self) -> Option<Ref<T>> {
+        self.head.as_ref().map(|node| {
+            Ref::map(node.borrow(), |node| &node.elem)
+        })
+    }
+
+    fn peek_front_mut(&mut self) -> Option<RefMut<T>> {
+        self.head.as_ref().map(|node| {
+            RefMut::map(node.borrow_mut(), |node| &mut node.elem)
+        })
+    }
+
+    fn peek_back(&self) -> Option<Ref<T>> {
+        self.tail.as_ref().map(|node| {
+            Ref::map(node.borrow(), |node| &node.elem)
+        })
+    }
+
+    fn peek_back_mut(&mut self) -> Option<RefMut<T>> {
+        self.tail.as_ref().map(|node| {
+            RefMut::map(node.borrow_mut(), |node| &mut node.elem)
+        })
+    }
+
+    fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
+        self.0.pop_front()
+    }
+}
+
+impl<T> DoubleEndedIterator for IntoIter<T> {
+    fn next_back(&mut self) -> Option<T> {
+        self.0.pop_back()
+    }
+}
+
+fn main() {
+    let mut list = List::new();
+    list.push_front(1); list.push_front(2); list.push_front(3);
+
+    let mut iter = list.into_iter();
+    assert_eq!(iter.next(), Some(3));
+    assert_eq!(iter.next_back(), Some(1));
+    assert_eq!(iter.next(), Some(2));
+    assert_eq!(iter.next_back(), None);
+    assert_eq!(iter.next(), None);
+}
--- LinearList/Rust/linearlist/src/main.rs
+++ LinearList/Rust/linearlist/src/main.rs
@@ -1,24 +1,17 @@
 #[derive(Debug)]
-struct Node {
-    next: Option<Box<Node>>,
-    data: Option<i32>,
+struct Node<T> {
+    next: Option<Box<Node<T>>>,
+    data: Option<T>,
 }
 
 #[derive(Debug)]
-struct List {
-    head: Option<Box<Node>>,
+struct List<T> {
+    head: Option<Box<Node<T>>>,
     length: u32,
 }
 
-impl Node {
-    fn new() -> Node {
-        Node {
-            next: None,
-            data: None,
-        }
-    }
-
-    fn new_with_data(d: i32) -> Node {
+impl<T> Node<T> {
+    fn new_with_data(d: T) -> Node<T> {
         Node {
             next: None,
             data: Some(d),
@@ -26,15 +19,15 @@ impl Node {
     }
 }
 
-impl List {
-    fn new() -> List {
+impl<T> List<T> {
+    fn new() -> List<T> {
         List {
             head: None,
             length: 0,
         }
     }
 
-    fn push(&mut self, d: i32) {
+    fn push(&mut self, d: T) {
         let mut node = Node::new_with_data(d);
         let head = self.head.take();
         match head {
@@ -53,7 +46,7 @@ impl List {
 }
 
 fn main() {
-    let mut list = List::new();
+    let mut list = List::<i32>::new();
     list.push(1);
     list.push(2);
     list.push(3);
